c24b255f5e82f08686b83591c262c33a211206d3
Theerapat Nugud <59160571@go.buu.ac.th>
2019-08-22 16:30:20 +0700

    Add click handlers to the buttons

--- app/src/main/java/buu/informatics/s59160571/colormyviews/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160571/colormyviews/MainActivity.kt
@@ -21,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
         R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
         R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+        R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+        R.id.yellow_buttton -> box_four_text.setBackgroundResource(R.color.my_yellow)
+        R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
         else -> view.setBackgroundColor(Color.LTGRAY)
     }
 
@@ -35,11 +38,16 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constan)
 
-        val ckickebleViews: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout)
+        val redButton = findViewById<TextView>(R.id.red_button)
+        val greenButton = findViewById<TextView>(R.id.green_button)
+        val yellowButton = findViewById<TextView>(R.id.yellow_buttton)
+
+        val ckickebleViews: List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout,redButton,yellowButton,greenButton)
         for(item in ckickebleViews){
             item.setOnClickListener { makeColored(it) }
         }
     }
+
 
 
 }
